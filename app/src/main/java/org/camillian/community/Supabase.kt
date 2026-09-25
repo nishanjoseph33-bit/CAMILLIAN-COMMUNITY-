5f41e490a305eb836e3605b3e9c672034e4f4b6e
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-25 10:27:25 +0530

    Add Supabase login foundation: app/src/main/java/org/camillian/community/Supabase.kt

---
+++ app/src/main/java/org/camillian/community/Supabase.kt
@@ -0,0 +1,15 @@
+package org.camillian.community
+
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+
+object Supabase {
+    val client = createSupabaseClient(
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_PUBLISHABLE_KEY
+    ) {
+        install(Auth)
+        install(Postgrest)
+    }
+}
