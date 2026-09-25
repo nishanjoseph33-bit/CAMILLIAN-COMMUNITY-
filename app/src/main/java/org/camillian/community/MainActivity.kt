4247e2e3bb195275dda4b1a12a4fff72db5ab69d
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-25 23:19:58 +0530

    Fix duplicate ActivityResultContracts import

--- app/src/main/java/org/camillian/community/MainActivity.kt
+++ app/src/main/java/org/camillian/community/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedContent
