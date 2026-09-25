b1c392e50219454305aaa7d27970fe3c3e0f3b42
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-25 11:28:50 +0530

    Compile Android app with SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 
 android {
     namespace = "org.camillian.community"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "org.camillian.community"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
 
