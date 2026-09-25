362a8aefa239cc7fa7675ed9c1ca36cb279dad36
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-25 16:14:56 +0530

    Fix Modifier.clip import for Android build

--- app/src/main/java/org/camillian/community/MainActivity.kt
+++ app/src/main/java/org/camillian/community/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clip
 import androidx.compose.foundation.shape.RoundedCornerShape
 import coil.compose.AsyncImage
 import androidx.compose.foundation.layout.*
@@ -32,6 +31,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
