0741698f8af9c401d066be5023183dc91d7133e0
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-25 10:31:00 +0530

    Update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
 }
+
+// Serialization plugin enabled for profile decoding.
