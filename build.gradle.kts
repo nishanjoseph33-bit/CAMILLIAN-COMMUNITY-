7775fa9e66b96aba55a49e10fb6d2ee57a02df6e
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-25 10:32:42 +0530

    Enable Kotlin serialization for Supabase profile decoding

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,5 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
 }
-
-// Serialization plugin enabled for profile decoding.
