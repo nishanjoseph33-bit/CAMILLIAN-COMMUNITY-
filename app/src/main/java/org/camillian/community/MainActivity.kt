c227a28dd88a6c1a06f0e42b3addca6625340533
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-25 23:26:05 +0530

    Fix profile crop image import

--- app/src/main/java/org/camillian/community/MainActivity.kt
+++ app/src/main/java/org/camillian/community/MainActivity.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.layout.ContentScale
