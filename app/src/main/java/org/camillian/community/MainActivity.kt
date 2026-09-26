10963c1a2a6de5c517cbbc823a08515fd3897c26
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-26 10:08:36 +0530

    Fix group member selection import

--- app/src/main/java/org/camillian/community/MainActivity.kt
+++ app/src/main/java/org/camillian/community/MainActivity.kt
@@ -31,6 +31,7 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import coil.compose.AsyncImage
 import androidx.compose.foundation.layout.*
