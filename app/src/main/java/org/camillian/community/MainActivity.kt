b7c013e1c7e321ae1d89de0c2f6c49395f1a2fb4
nishanjoseph33-bit <nishanjoseph33@gmail.com>
2026-09-26 11:15:43 +0530

    Fix missing combinedClickable import for message actions

--- app/src/main/java/org/camillian/community/MainActivity.kt
+++ app/src/main/java/org/camillian/community/MainActivity.kt
@@ -32,6 +32,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.foundation.shape.RoundedCornerShape
